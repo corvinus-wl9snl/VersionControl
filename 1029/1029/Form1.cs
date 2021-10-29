e36683587a498fcae44665891e1b6de6b0309492
Vazul Tasi <daniel.tasi@stud.uni-corvinus.hu>
2021-10-29 20:45:37 +0200

    3

--- 1029/1029/Form1.cs
+++ 1029/1029/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1029.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,26 @@ namespace _1029
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            WebCall();
+        }
+
+        void WebCall()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
