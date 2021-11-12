460222d658b9414f50560c4a5e7912cc867bd4eb
Vazul Tasi <daniel.tasi@stud.uni-corvinus.hu>
2021-11-12 21:53:31 +0100

    4

---
+++ 1112/1112/Abstractions/Toy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1112.Abstractions
+{
+    public abstract class Toy : Label
+    {
+            public Toy()
+            {
+                AutoSize = false;
+                Width = 50;
+                Height = 50;
+                Paint += Ball_Paint;
+            }
+
+            private void Ball_Paint(object sender, PaintEventArgs e)
+            {
+                DrawImage(e.Graphics);
+            }
+
+        protected abstract void DrawImage(Graphics g);
+
+            public void MoveBall()
+            {
+                Left++;
+            }
+    }
+}
