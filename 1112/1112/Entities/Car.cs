5e0ba0f5307f89f84f59b559044977d9ee340461
Vazul Tasi <daniel.tasi@stud.uni-corvinus.hu>
2021-11-12 22:26:54 +0100

    5

---
+++ 1112/1112/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using _1112.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1112.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
