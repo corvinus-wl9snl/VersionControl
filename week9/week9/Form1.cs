71df979c1193e1abdde7bf2fdb5f4d8678f751f6
Vazul Tasi <daniel.tasi@stud.uni-corvinus.hu>
2021-12-03 22:36:37 +0100

    ..

--- week9/week9/Form1.cs
+++ week9/week9/Form1.cs
@@ -14,6 +14,7 @@ namespace week9
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
@@ -24,6 +25,29 @@ namespace week9
             Population = ReadPopulaiton(@"C:\Temp\nép.csv");
             BirthProbabilities = ReadBirth(@"C:\Temp\születés.csv");
             DeathProbabilities = ReadDeath(@"C:\Temp\halál.csv");
+            Simulation();
+        }
+
+        private void Simulation()
+        {
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<DeathProbability> ReadDeath(string filename)
@@ -38,7 +62,7 @@ namespace week9
                     {
                         Age = int.Parse(line[0]),
                         Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
-                        P = double.Parse(line[3])
+                        P = double.Parse(line[2])
                     });
                 }    
             }
@@ -57,7 +81,7 @@ namespace week9
                     {
                         Age = int.Parse(line[0]),
                         NbrOfChildren = int.Parse(line[1]),
-                        P = double.Parse(line[3])
+                        P = double.Parse(line[2])
                     });
                 }
             }
@@ -84,6 +108,42 @@ namespace week9
                 }
             }
             return population;
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+            if (!person.IsAlive) return;
+
+            // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+            byte age = (byte)(year - person.BirthYear);
+
+            // Halál kezelése
+            // Halálozási valószínűség kikeresése
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+            // Meghal a személy?
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                //Szülési valószínűség kikeresése
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                //Születik gyermek?
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
     }
 }
